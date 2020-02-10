a5f5525c9ac3238e1c167f2864d0d19e0c5364ab
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2020-02-10 15:24:54 +0530

    Update upload_to_s3.sh

--- scripts/upload_to_s3.sh
+++ scripts/upload_to_s3.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -ex
 aws configure set prd.region us-west-2
 
-if [ "${GIT_BRANCH}" == "master" ]; then
+if [ "${GIT_BRANCH}" == "origin/master" ]; then
     PROFILE="prd"
     REGION="us-west-2"
     rm -f target/*tests.jar
     for AWS_ENVIRONMENT in ${AWS_ENVIRONMENTS}; do
         aws --profile ${PROFILE} --region=${REGION} s3 cp target/myapp-*.war s3://stsureshbab-987/myapp-${GIT_COMMIT:0:8}.war
     done
-elif [ "${GIT_BRANCH}" == "develop" ]; then
+elif [ "${GIT_BRANCH}" == "origin/develop" ]; then
     PROFILE="dev"
     ls
 fi
