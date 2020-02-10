304a0fadc205d1f79da0d8fac304e5b8e854d59e
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2020-02-10 15:34:34 +0530

    Update upload_to_s3.sh

--- scripts/upload_to_s3.sh
+++ scripts/upload_to_s3.sh
@@ -6,7 +6,7 @@
 if [ "${GIT_BRANCH}" == "origin/master" ]; then
     PROFILES="prd"
     REGION="us-west-2"
-    aws --region=${REGION} s3 cp target/myapp-*.war s3://stsureshbab-987/myapp-${GIT_COMMIT:0:8}.war
+    aws --region=${REGION} s3 cp target/myapp-*.war s3://stsureshbab-987/myapp/myapp-${GIT_COMMIT:0:8}.war
 elif [ "${GIT_BRANCH}" == "origin/develop" ]; then
     PROFILE="dev"
     ls
