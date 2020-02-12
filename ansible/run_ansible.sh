383fd0e2fdcbff4440f458b65fb5400ac24874d7
suresh <suresh@gmail.com>
2020-02-12 15:23:03 +0530

    ansible

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -4,7 +4,9 @@
 ansible all -m ping -i ./ansible/inventory
 # ansible all -i ./ansible/inventory -m yum -a "name=git state=present" --become
 
+
 cd ansible/
+rm -rf ansible-modules || true
 git clone https://${GIT_PRIVATE}@github.com/suresh-devops/ansible-modules.git
 cd ansible-modules/
 git checkout master
