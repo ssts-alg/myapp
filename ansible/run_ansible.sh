bc9453367463807a0e018d3480e4183e0cded50b
suresh <suresh@gmail.com>
2020-02-12 13:30:52 +0530

    ansible

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -1,2 +1,2 @@
 ansible all -m ping -i ./ansible/inventory
-ansible all -i ./ansible/inventory -a "name=git state=present" --become
+ansible all -i ./ansible/inventory -m yum -a "name=git state=present" --become
