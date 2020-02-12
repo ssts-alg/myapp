b64ae85b90c17a8a448ed3b39641cfa747abc3ce
suresh <suresh@gmail.com>
2020-02-12 14:15:08 +0530

    ansible

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -1,5 +1,15 @@
 ansible all -m ping -i ./ansible/inventory
 # ansible all -i ./ansible/inventory -m yum -a "name=git state=present" --become
+
+cd ansible/
+git clone git@github.com:suresh-devops/ansible-modules.git
+cd ansible-modules/
+git checkout master
+git pull origin master
+cd ../../
+
+
+
 if [ `echo $?` -eq 0 ]
 then
    ansible-playbook -i ./ansible/inventory ./ansible/install.yml --become
