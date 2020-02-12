7c50aecb2036d7aa16ba9bb845a3486e422acd6e
suresh <suresh@gmail.com>
2020-02-12 13:34:01 +0530

    ansible

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -1,2 +1,3 @@
 ansible all -m ping -i ./ansible/inventory
-ansible all -i ./ansible/inventory -m yum -a "name=git state=present" --become
+# ansible all -i ./ansible/inventory -m yum -a "name=git state=present" --become
+ansible-playbook -i ./ansible/inventory ./install/yml --become
