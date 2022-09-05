e3e84a0d68bfe32238b7ab7f8f51dfd97bf5b4c2
sstechnosolutions <sstechnosolutions3@gmail.com>
2022-09-05 15:46:09 +0530

    added Jenkinsfile

--- kubernetes/run_deployment.sh
+++ kubernetes/run_deployment.sh
@@ -2,7 +2,7 @@
 
 project_version=$(echo $PROJECT_VERSION)
 # PROJECT_VERSION=$(cat pom.xml | grep "version" | head -1 | awk '{print $1}' |  sed "s/<version>//" | sed "s/<.*//")
-sed  "s/PROJECTVERSION/$project_version/g" kubernetes/deployment.yaml > kubernetes/deployment1.yaml
-cat kubernetes/deployment1.yaml
-kubectl apply -f kubernetes/deployment1.yaml
+sed -i "s/PROJECTVERSION/$project_version/g" kubernetes/deployment.yaml
+cat kubernetes/deployment.yaml
+kubectl apply -f kubernetes/deployment.yaml
 # kubectl apply -f kubernetes/deployment.yaml
